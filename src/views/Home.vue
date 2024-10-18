b9702059b21fc65b58b9281234d290db0464c0b7
fxdamassa <afagundes777@gmail.com>
2024-10-17 22:05:01 -0300

    Criado telas de home, login, calendar para o google calendar

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Bem-vindo ao BarberApp!</h1>
+    <router-link to="/login">Fazer Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
