c0c47a3fda4ad3ba6ca3b6bff5ec7512d90dfa6f
andreflik <andreflik@gmail.com>
2024-12-03 21:03:22 -0300

    feature(UserLogin): Realizado ajuste no login via google

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center h-screen bg-gray-100">
     <div class="w-full max-w-sm bg-white shadow-lg rounded-lg p-8">
-      <h1 class="text-2xl font-bold text-center mb-6">Marquinhos BarberShop</h1>
+      <h1 class="text-2xl font-bold text-center mb-6">Marquinhos Barbershop</h1>
       <button
           @click="redirectToGoogle"
           class="flex items-center justify-center w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition"
@@ -15,19 +15,18 @@
 
 <script>
 export default {
-  name: 'UserLogin',
+  name: "UserLogin",
   methods: {
     redirectToGoogle() {
-      window.location.href = 'http://127.0.0.1:8000/api/google/redirect';
+      window.location.href = "http://127.0.0.1:8000/api/google/redirect";
     },
-  },
-  created() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get('token');
-    if (token) {
-      localStorage.setItem('auth_token', token);
-      this.$router.push({ name: 'UserDashboard' });
-    }
   },
 };
 </script>
+
+<style scoped>
+.container {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
