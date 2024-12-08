a31dfbadc99613be7ebf7a33e4c96be5d0b33c97
andreflik <andreflik@gmail.com>
2024-12-08 15:23:54 -0300

    feature(UserLogin):Atualizado função redirecttoGoogle para loginWithGoogle

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -3,7 +3,7 @@
     <div class="w-full max-w-sm bg-white shadow-lg rounded-lg p-8">
       <h1 class="text-2xl font-bold text-center mb-6">Marquinhos Barbershop</h1>
       <button
-          @click="redirectToGoogle"
+          @click="loginWithGoogle"
           class="flex items-center justify-center w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition"
       >
         <i class="fab fa-google mr-2"></i>
@@ -17,7 +17,7 @@
 export default {
   name: "UserLogin",
   methods: {
-    redirectToGoogle() {
+    loginWithGoogle() {
       window.location.href = "http://127.0.0.1:8000/api/google/redirect";
     },
   },
